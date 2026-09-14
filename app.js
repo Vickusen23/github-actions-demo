0c486b078df95cfd5a6a83a558846e9daf2ed5f3
Vickusen23 <victorusen23@gmail.com>
2026-09-14 13:51:32 +0100

    Added multiplications, substractions, divisions

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
+// math.js
+
 function add(a, b) {
   return a + b;
 }
 
-module.exports = add;
+function subtract(a, b) {
+  return a - b;
+}
 
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error("Division by zero is not allowed");
+  }
+  return a / b;
+}
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide
+};
